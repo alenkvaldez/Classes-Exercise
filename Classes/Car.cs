a4ca597dc2e7156a16083586f964219b6a64f468
Alen Valdez <alenkvaldeztch@gmail.com>
2023-02-01 15:08:38 -0800

    1

---
+++ Classes/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Classes
+{
+	public class Car
+	{
+		public Car()
+		{
+
+		}
+
+
+
+
+
+
+		public Car(string make, string model, int year)
+		{
+			Make = "";
+			Model = "";
+			Year = year;
+		}
+
+
+		public string Make { get; set; }
+		public string Model { get; set; }
+		public int Year { get; set; }
+	}
+}
+
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "acura";
+            myCar.Model = "tlx";
+            myCar.Year = 2018;
+
+            var lexus = new Car()
+            {
+                Make = "lexus",
+                Model = "rx450",
+                Year = 2023
+            };
+
+        var honda = new Car("civic", "hatchback", 2021);
+        var carList = new List<Car>() { myCar, lexus, honda };
+
+            foreach (var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
         }
     }
 }
